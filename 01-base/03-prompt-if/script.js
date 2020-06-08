6731d518a8516f01ea7b1539c51aba23ececefb4
Huyen059 <samihuyen059@gmail.com>
2020-06-08 10:06:07 +0200

    add files and do 01-base exercises

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+  // your code here
+  let answer = prompt('Do you want some cake?', 'Yes');
+  
+  if (answer == 'Yes' || answer == 'yes'){
+    alert('Congrat! You made the right choice.');
+  } else {
+    alert('More cake for me then!');
+  }
+
+})();
---
+++ 01-base/04-asv-confirm/script.js
@@ -0,0 +1,25 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function mustConfirm() {
+
+    // your code here
+    let age = prompt('How old are you?', '18');
+    let gender = prompt('What is your gender?', 'male');
+    let town = prompt('What is the name of the town you are living now?', 'London');
+
+    if(confirm(`You are ${age} years old, ${gender} and live in ${town}. Is it correct?`)){
+        alert('Thanks for comfirming.');
+    } else {
+        mustConfirm();
+    }
+
+})();
